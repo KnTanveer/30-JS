7474eaf62033a5ac2245c0471b70f0a6868f7e40
Tanveer Khan <tanveer@Tanveers-MacBook-Air.local>
2025-12-31 11:47:36 +0530

    palidnrome checker

---
+++ palindrome checker/app.js
@@ -0,0 +1,17 @@
+const input = document.getElementById("textInput");
+
+function check() {
+    const reverse = input.value.split("").reverse().join("")
+    
+    if (reverse === input.value) {
+        alert(input.value + " is a palindrome")
+    } else {
+        alert(input.value + " is not a palindrome")
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        check();
+    }
+});
