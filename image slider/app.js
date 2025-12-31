8e166a52c63e999b172ff09f4683c8ffa1e19f7c
Tanveer Khan <tanveer@Tanveers-MacBook-Air.local>
2025-12-31 18:13:26 +0530

    image slidder

---
+++ image slider/app.js
@@ -0,0 +1,19 @@
+let container = document.querySelector(".gallery")
+const backBtn = document.getElementById("backBtn")
+const nextBtn = document.getElementById("nextBtn")
+
+container.addEventListener("wheel", (event) => {
+    event.preventDefault();
+    container.scrollLeft += event.deltaY;
+    container.style.scrollBehaviour = "auto";
+});
+
+nextBtn.addEventListener("click", () => {
+    container.scrollLeft += 600;
+    container.style.scrollBehaviour = "smooth";
+});
+
+backBtn.addEventListener("click", () => {
+    container.scrollLeft -= 600;
+    container.style.scrollBehaviour = "smooth";
+});
